44acd335746eb67b077712da35a513fd4957cd3e
hungnguyen2510 <htmm2510@gmail.com>
2020-11-14 11:44:37 +0700

    fix navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,38 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
+        <v-list-item to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-title>Home</v-list-item-title>
         </v-list-item>
+
+        <v-list-group :value="true" prepend-icon="mdi-account-circle">
+          <template v-slot:activator>
+            <v-list-item-title>Danh Sách</v-list-item-title>
+          </template>
+
+          <v-list-item
+            v-for="(item, i) in admins"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
@@ -37,41 +42,23 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
-
 <script>
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Home',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Management',
-          to: '/management'
-        },
-        {
-          icon: 'mdi-login',
-          title: 'Logout',
-          to: '/#'
-        }
-      ],
-      title: 'School Lookup'
-    }
-  }
-}
+  data: () => ({
+    clipped: false,
+    drawer: false,
+    fixed: false,
+    title: "School Lookup",
+    admins: [
+      {
+        icon: "mdi-apps",
+        title: "DS Trường",
+        to: "/ds-truong"
+      },
+      { icon: "mdi-apps", title: "DS Ngành", to: "/test02" }
+    ]
+  })
+};
 </script>
